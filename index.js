fb41f78ccff8dc43c6668dcb10e1e61b81dc4d39
Your Name <you@example.com>
2024-01-21 16:12:15 +0600

    jwt

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 require('dotenv').config();
+const jwt = require('jsonwebtoken');
 const port = process.env.PORT || 5000;
 
 // middleware
@@ -31,8 +32,34 @@ async function run() {
         const userCollection = client.db('popular-news').collection('users');
 
 
+        // jwt api 
+        app.post('/jwt', async (req, res) => {
+            const user = req.body;
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+                expiresIn: '10h'
+            });
+            res.send({ token })
+        })
+
+        // middleware
+        const verifyToken = (req, res, next) => {
+            console.log("inside verify token", req.headers.authorization);
+            if (!req.headers.authorization) {
+                return res.status(401).send({ message: 'unauthorized access' });
+            }
+
+            const token = req.headers.authorization.split(' ')[1];
+            jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+                if (err) {
+                    return res.status(401).send({ message: 'unauthorized access' });
+                }
+                req.decoded = decoded
+                next()
+            })
+
+        }
         // user related api
-        app.get('/users', async (req, res) => {
+        app.get('/users', verifyToken, async (req, res) => {
             // console.log("inside verify token", req.headers);
             const result = await userCollection.find().toArray();
             res.send(result)
@@ -76,7 +103,7 @@ async function run() {
             const filter = req.query;
             const page = parseInt(filter.page)
             const size = parseInt(filter.size)
-            console.log(filter, page, size)
+            // console.log(filter, page, size)
             let query = {}
             if (req.query.search) {
                 query = { title: { $regex: filter.search, $options: 'i' } }
